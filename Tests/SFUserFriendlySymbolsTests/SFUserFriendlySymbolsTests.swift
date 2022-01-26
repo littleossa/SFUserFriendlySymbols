c8dc55135bdab7c7ae3c330461a5635655fb0f8f
littleossa <littleossa@gmail.com>
2022-01-26 16:34:24 +0900

    [refactor] remove unneccessary code from SFUserFriendlySymbolTest

--- Tests/SFUserFriendlySymbolsTests/SFUserFriendlySymbolsTests.swift
+++ Tests/SFUserFriendlySymbolsTests/SFUserFriendlySymbolsTests.swift
@@ -2,10 +2,4 @@
     @testable import SFUserFriendlySymbols
 
     final class SFUserFriendlySymbolsTests: XCTestCase {
-        func testExample() {
-            // This is an example of a functional test case.
-            // Use XCTAssert and related functions to verify your tests produce the correct
-            // results.
-            XCTAssertEqual(SFUserFriendlySymbols().text, "Hello, World!")
-        }
     }
