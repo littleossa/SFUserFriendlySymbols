b48cb21d2049ba3b17cc753895a517802a49e10d
littleossa <littleossa@gmail.com>
2022-01-26 21:41:43 +0900

    [feat] add platform on package.swift

--- Package.swift
+++ Package.swift
@@ -5,19 +5,19 @@ import PackageDescription
 
 let package = Package(
     name: "SFUserFriendlySymbols",
+    platforms: [
+        .iOS(.v13),
+        .macOS(.v11),
+        .tvOS(.v13),
+        .watchOS(.v6),
+    ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "SFUserFriendlySymbols",
             targets: ["SFUserFriendlySymbols"]),
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
+    dependencies: [],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SFUserFriendlySymbols",
             dependencies: []),
