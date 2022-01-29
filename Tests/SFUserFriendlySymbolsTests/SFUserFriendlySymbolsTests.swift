799d404f06974ee6f3402c9efb1423998e3a98d1
littleossa <littleossa@gmail.com>
2022-01-29 22:16:19 +0900

    [test] Add tests are All cases test and initializer tests

--- Tests/SFUserFriendlySymbolsTests/SFUserFriendlySymbolsTests.swift
+++ Tests/SFUserFriendlySymbolsTests/SFUserFriendlySymbolsTests.swift
@@ -1,5 +1,39 @@
-    import XCTest
-    @testable import SFUserFriendlySymbols
+import XCTest
+#if canImport(UIKit)
+import UIKit
+#elseif canImport(Cocoa)
+import Cocoa
+#endif
 
-    final class SFUserFriendlySymbolsTests: XCTestCase {
+@testable import SFUserFriendlySymbols
+import SwiftUI
+
+final class SFUserFriendlySymbolsTests: XCTestCase {
+    
+    /// test SF Symbols all cases exist or not
+    func testSFSymbolsAllCases() {
+        for symbol in SFSymbols.allCases {
+#if os(iOS)
+            let image = UIImage(symbol: symbol)
+            XCTAssertNotNil(image, "This symbos is not available: \(symbol)")
+#elseif os(macOS)
+            let image = NSImage(symbol: symbol)
+            XCTAssertNotNil(image, "This symbos is not available: \(symbol)")
+#endif
+        }
     }
+    
+    /// test each init functions succeed or not
+    func testInitializeSFSymbolsImage() {
+#if os(iOS)
+        let uiImage = UIImage(symbol: .carFill)
+        XCTAssertNotNil(uiImage)
+        
+        let image = Image(symbol: .pencil)
+        XCTAssertNotNil(image)
+#elseif os(macOS)
+        let nsImage = NSImage(symbol: .trash)
+        XCTAssertNotNil(nsImage)
+#endif
+    }
+}
